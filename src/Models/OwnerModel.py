ed87f99b11c8948568d464d09828a7756d884262
JoelCunningham <s3949308@student.rmit.edu.au>
2025-06-27 00:06:04 +1000

    Removes redundant id

--- src/Models/OwnerModel.py
+++ src/Models/OwnerModel.py
@@ -1,11 +1,10 @@
-from sqlmodel import Field, Relationship
+from sqlmodel import Relationship
 
 from Models.BaseModel import BaseModel
 from Models.DeviceModel import Device
 
 
 class Owner(BaseModel, table=True):
-    id: int = Field(default=None, primary_key=True)
     name: str
     
     devices: list[Device] = Relationship(back_populates="owner")
