4d31828e0004776fd7e3681a34b28976164a93f4
JoelCunningham <s3949308@student.rmit.edu.au>
2025-09-07 19:45:39 +1000

    Implements status panel features

--- frontend/src/app/components/partials/status-panel.ts
+++ frontend/src/app/components/partials/status-panel.ts
@@ -1,16 +1,26 @@
 import { Icon } from '#components/common/icon';
+import { DeviceService } from '#services/device-service';
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-status-panel',
-  imports: [Icon],
+  imports: [Icon, DatePipe],
   templateUrl: './status-panel.html',
   styleUrl: './status-panel.scss',
 })
 export class StatusPanel {
-  isLiveView = true;
+  protected isLiveView = true;
+  protected lastScan: Date | null = null;
+
+  constructor(private deviceService: DeviceService) {
+    this.deviceService.lastRefresh.subscribe((date) => {
+      this.lastScan = date;
+    });
+  }
 
   toggleViewMode() {
     this.isLiveView = !this.isLiveView;
+    this.deviceService.setAutoRefresh(this.isLiveView);
   }
 }
--- frontend/src/app/services/device-service.ts
+++ frontend/src/app/services/device-service.ts
@@ -6,22 +6,28 @@ import { BehaviorSubject, interval, Observable, Subscription } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class DeviceService {
   private apiUrl = 'http://localhost:5000/api/devices';
+  private doRefresh = true;
+
   private devicesSubject = new BehaviorSubject<Device[]>([]);
+  private lastRefreshSubject = new BehaviorSubject<Date | null>(null);
 
   public devices = this.devicesSubject.asObservable();
+  public lastRefresh = this.lastRefreshSubject.asObservable();
 
   constructor(private http: HttpClient) {
     this.loadDevices();
 
     interval(60000).subscribe(() => {
-      this.loadDevices();
-      console.log('Devices updated');
+      if (this.doRefresh) {
+        this.loadDevices();
+      }
     });
   }
 
   private loadDevices(): void {
     this.http.get<Device[]>(this.apiUrl).subscribe((devices) => {
       this.devicesSubject.next(devices);
+      this.lastRefreshSubject.next(new Date());
     });
   }
 
@@ -41,7 +47,10 @@ export class DeviceService {
     return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 
-  refreshDevices(): void {
-    this.loadDevices();
+  setAutoRefresh(enabled: boolean): void {
+    this.doRefresh = enabled;
+    if (enabled) {
+      this.loadDevices();
+    }
   }
 }
