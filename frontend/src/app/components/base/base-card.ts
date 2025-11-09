e4efe57c8fa12efdab8fdf3036b34875b366366a
JoelCunningham <s3949308@student.rmit.edu.au>
2025-11-09 18:44:58 +1100

    Removes remove button from base card

--- frontend/src/app/components/base/base-card.ts
+++ frontend/src/app/components/base/base-card.ts
@@ -1,9 +1,7 @@
-import { Icon } from '#components/common/icon';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-base-card',
-  imports: [Icon],
   templateUrl: './base-card.html',
   styleUrl: './base-card.scss',
 })
@@ -13,19 +11,13 @@ export class BaseCard<T> {
 
   @Input() primary: boolean = false;
   @Input() selected: boolean = false;
-  @Input() removable: boolean = false;
   @Input() interactive: boolean = true;
 
   @Input() fullWidth: boolean = false;
 
   @Output() onClick = new EventEmitter<T | null>();
-  @Output() onRemove = new EventEmitter<T | null>();
 
   click() {
     this.onClick.emit(this.value);
-  }
-
-  remove() {
-    this.onRemove.emit(this.value);
   }
 }
